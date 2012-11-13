1bd7d5d41f77697b07fe0fa64e7a2f7f02821eb5
Markus Döring <markus.b.doering@gmail.com>
2012-11-13 18:49:31 +0100

    schleife für c_in; alles viel schöner gemacht

--- tnn/ex3/aufgabe3.m
+++ tnn/ex3/aufgabe3.m
@@ -16,53 +16,76 @@ RefZeitSchritte = 20;
 anzahl_neuronen = 200;
 dX = 1;
 input_range = 8:dX:20;
+mittleres_potential = zeros(size(input_range));
 
 cin_range = 0:.1:2;
+selektion = [2 5 10 15 20];
+coli = 0;
+col = lines;
+leg = cell(length(selektion),1);
 
-
-cin = .55;
-% Berechne die Eingabe für diesen Eingabewert
-x = 11 + 4*rand(anzahl_neuronen,AnzGesSchritte) - 2;
-
-% haben ein Anfangspotential und dann nach jedem Schritt ein neues, daher die +1
-u = zeros(anzahl_neuronen+1,AnzGesSchritte + 1); % Init Membranpotential
-% dendritische potentiale: startwerte
-u(:,1) = rand(anzahl_neuronen+1,1)*10;
-y = zeros(anzahl_neuronen,AnzGesSchritte + 1); % Init Axonales Potential
-theta = thetaREST * ones(anzahl_neuronen,AnzGesSchritte+1); % Init Schwellenpotential
-Uk = [ones(100,1)*60;ones(100,1)*(-20)];
-c = [ones(100,1)*1.2;ones(100,1)*cin];
-r = zeros(anzahl_neuronen,1);
-
-%Variablen, die geaendert werden muessen
-letzter_spike = NaN(anzahl_neuronen,1);
-
-for step = 1:AnzGesSchritte
-	% schleife über die neuronen im NN
-	for neuron = 1:anzahl_neuronen
-		
-		% prüfe wie die schwelle liegt
-		if step - letzter_spike(neuron) <= 20 % ist false wenn NaN vorkommt
-			theta(neuron,step) = thetaABS;
-		elseif ~isnan(letzter_spike(neuron))
-			theta(neuron,step) = 100/(step - letzter_spike(neuron) -10) + 10;
+subplot(1,2,1)
+for k=1:length(cin_range)
+	cin = cin_range(k);
+	
+	% Berechne die Eingabe
+	x = 11 + 4*rand(anzahl_neuronen,AnzGesSchritte) - 2;
+	
+	% haben ein Anfangspotential und dann nach jedem Schritt ein neues, daher die +1
+	u = zeros(anzahl_neuronen+1,AnzGesSchritte + 1); % Init Membranpotential
+	% dendritische potentiale: startwerte
+	u(:,1) = rand(anzahl_neuronen+1,1)*10;
+	y = zeros(anzahl_neuronen,AnzGesSchritte + 1); % Init Axonales Potential
+	theta = thetaREST * ones(anzahl_neuronen,AnzGesSchritte+1); % Init Schwellenpotential
+	Uk = [ones(100,1)*60;ones(100,1)*(-20)];
+	c = [ones(100,1)*1.2;ones(100,1)*cin];
+	letzter_spike = NaN(anzahl_neuronen,1);
+	
+	for step = 1:AnzGesSchritte
+		% schleife über die neuronen im NN
+		for neuron = 1:anzahl_neuronen
+			
+			% prüfe wie die schwelle liegt
+			if step - letzter_spike(neuron) <= 20 % ist false wenn NaN vorkommt
+				theta(neuron,step) = thetaABS;
+			elseif ~isnan(letzter_spike(neuron))
+				theta(neuron,step) = 100/(step - letzter_spike(neuron) -10) + 10;
+			end
+			
+			% prüfe ob spike auftritt
+			if  u(neuron,step) >= theta(neuron,step)
+				y(neuron,step) = 1;
+				letzter_spike(neuron) = step;
+			end
+			
+			% update dendritisches potential
+			u(neuron,step+1) = (1.0-rho)*u(neuron,step) + rho*x(neuron,step);
+			
 		end
 		
-		% prüfe ob spike auftritt
-		if  u(neuron,step) >= theta(neuron,step)
-			y(neuron,step) = 1;
-			letzter_spike(neuron) = step;
-		end
+		% wie weit zurück müssen wir schauen?
+		minind=max(1,step-10);
+		% response ergibt sich aus .05 pro spike in den letzten 10 Schritten
+		response = sum(y(:,minind:step), 2)*.05;
 		
-		% update dendritisches potential
-		u(neuron,step+1) = (1.0-rho)*u(neuron,step) + rho*x(neuron,step);
-		
+		% Spezialbehandlung für unser Neuron
+		u(201,step+1) = (1.0-rho)*u(201,step) + sum(Uk.*c.*response); %+ rho*x(neuron,step);
 	end
 	
-	% hier wird unser spezielles neuron behandelt
-	m=max(1,step-10);
-	r = sum(y(:,m:step), 2)*.05;
-	u(201,step+1) = (1.0-rho)*u(201,step) + sum(Uk.*c.*r); %+ rho*x(neuron,step);
+	if any(k==selektion)
+		hold on
+		coli = coli+1;
+		plot(u(201,:), 'Color', col(coli, :))
+		leg{coli} = sprintf('c_{in} = %f', cin); 
+	end
+	mittleres_potential(k) = mean(u(201,:));
 end
 
-plot(u(201,:))
+xlabel('Zeit');
+ylabel('dendritisches Potential');
+legend(leg, 'Location', 'SE');
+
+subplot(1,2,2)
+plot(cin_range, mittleres_potential)
+xlabel('c_{in}');
+ylabel('mittleres Potential');
