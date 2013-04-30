5d1d23753f1eef4da907711fd2d65874f40e0042
Markus Döring <webmaster@burgerdev.de>
2013-04-30 10:38:46 +0200

    test for L2 norm

--- image_analysis/ex2/test.py
+++ image_analysis/ex2/test.py
@@ -1,5 +1,17 @@
 from textureSynthesis_helpers import mkCostMap
 import numpy
+from vigra.impex import readImage, writeImage
+
+
+import matplotlib 
+matplotlib.use("Qt4Agg")
+from mpl_toolkits.mplot3d import Axes3D 
+from matplotlib import pyplot as plot
+
+def showimg(x):
+    x = x.astype(numpy.uint8)
+    x = x.swapaxes(0,1)
+    plot.imshow(x)
 
 def p(s,a):
     print('---------------')
@@ -8,10 +20,27 @@ def p(s,a):
     print('---------------\n')
 
 
-if __name__ == "__main__":
-    x = numpy.asarray([[[2,3,4],[7,8,7]],[[1,5,2], [7,3,4]]]).astype(numpy.float32)
-    y = x * 0.5
+def testMkCostMap():
+    X = readImage('circ.png')
+    Y = X
+    Y = numpy.roll(Y,20,axis=0)
+    Y = numpy.roll(Y,10,axis=1)
+    
+    D = mkCostMap(X,Y)
+    
+    plot.subplot(2,3,1)
+    showimg(X)
 
-    z = mkCostMap(x,y)
+    plot.subplot(2,3,2)
+    showimg(Y)
+
+    plot.subplot(2,3,3)
+    showimg(D)
+    plot.gray()
+    
+    plot.show()
+
+if __name__ == "__main__":
+    testMkCostMap()
 
 
