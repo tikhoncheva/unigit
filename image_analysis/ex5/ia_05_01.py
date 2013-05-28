e472451db99a0155d116fde57cb8ef0e21114a38
burgerdev <webmaster@burgerdev.de>
2013-05-28 21:28:55 +0200

    clarifications

--- image_analysis/ex5/ia_05_01.py
+++ image_analysis/ex5/ia_05_01.py
@@ -5,7 +5,6 @@
 # File: ia_05_01.py
 #
 
-
 import vigra
 import numpy as np
 
@@ -13,46 +12,67 @@ import matplotlib
 matplotlib.use('Qt4Agg')
 from matplotlib import pyplot as plot
 
-from pprint import pprint as pp
-
 def readvid():
+    '''
+    read video from hdf5 file
+    '''
     name = "cctv_video.h5"
     vol = vigra.readHDF5(name, 'data')
     return vol
 
 def vid2pngs(vid, prefix="image_"):
+    '''
+    save video as png images
+    '''
+    
+    # use a loop since the documentation on vigra.impex.writeSlices is unclear
     for t in range(vid.shape[2]):
         vigra.impex.writeImage(vid[...,t],"tmp/%s%03d.png" % (prefix,t,))
     
 def ex1():
+    '''
+    solve excercise 1
+    '''
+    
+    # number of sub-excercise
+    sub = 3
+    
     vol = readvid()
+    
+    # calculate mean and median image
     meanim = np.mean(vol,axis=2)
     medim = np.median(vol, axis=2)
-    '''
-    plot.subplot(1,2,1)
-    plot.imshow(meanim.swapaxes(0,1))
-    plot.gray()
-    plot.title("Mean Image")
     
-    plot.subplot(1,2,2)
-    plot.imshow(medim.swapaxes(0,1))
-    plot.gray()
-    plot.title("Median Image")
+    if sub == 2: # show mean and median image
+        plot.subplot(1,2,1)
+        plot.imshow(meanim.swapaxes(0,1))
+        plot.gray()
+        plot.title("Mean Image")
         
-    plot.show()
-    '''
-    
-    volmean = vol-meanim.reshape((meanim.shape[0], meanim.shape[1],1)) + 255
-    volmed = vol-medim.reshape((medim.shape[0], medim.shape[1],1)) + 255
-    
-    volmean = volmean / max(volmean.max(),volmed.max()) * 255
-    volmed = volmed / max(volmean.max(),volmed.max()) * 255
-    
-    vid = np.concatenate((volmean,volmed))
-    
-    vid2pngs(vid)
+        plot.subplot(1,2,2)
+        plot.imshow(medim.swapaxes(0,1))
+        plot.gray()
+        plot.title("Median Image")
+            
+        plot.show()
+    else: # generate a video with mean / median subtracted
+        # subtract 
+        volmean = vol-meanim.reshape((meanim.shape[0], meanim.shape[1],1)) + 255
+        volmed = vol-medim.reshape((medim.shape[0], medim.shape[1],1)) + 255
+        
+        # normalize by maximum of both
+        totalmax = max(volmean.max(),volmed.max())
+        volmean = volmean / totalmax * 255
+        volmed = volmed / totalmax * 255
+        
+        # show the videos next to each other
+        vid = np.concatenate((volmean,volmed))
+        
+        vid2pngs(vid)
+        
+        # final video generation steo is done by mencoder via bash script
+        
     
 if __name__ == "__main__":
-    pass
     ex1()
     
---
+++ image_analysis/ex5/ia_05_02.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# coding: utf-8
+# 
+# Author: Markus Doering
+# File: ia_05_02.py
+#
+
+import vigra
+import numpy as np
+
+import matplotlib
+matplotlib.use('Qt4Agg')
+from matplotlib import pyplot as plot
+
+
