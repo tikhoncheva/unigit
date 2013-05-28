9ef2f0c278a993ff104595bde044f291f8070566
Markus Döring <webmaster@burgerdev.de>
2013-05-28 16:58:16 +0200

    solved 1.2, 1.3 waiting for better cpu

--- image_analysis/ex5/1.py
+++ image_analysis/ex5/1.py
@@ -12,46 +12,40 @@ from pprint import pprint as pp
 def readvid():
     name = "cctv_video.h5"
     vol = vigra.readHDF5(name, 'data')
-    return vigra.VigraArray(vol,axistags=vigra.defaultAxistags('xyt'))
-    
-    
-def meanimage(im):
-    t_index = -1
-    for k in range(len(im.axistags)):
-        if im.axistags[k].isTemporal():
-            t_index = k
-    
-    assert not t_index < 0, "No time axis present."
-    
-    m = im.mean(axis=t_index)
-    
-    return m
-    
-def medianimage(im):
-    return meanimage(im)
-    
-    
-def test():
-    vol = readvid()
-    print(vol.shape,vol.axistags)
-    plot.imshow(vol[:,:,0])
-    plot.gray()
-    plot.show()
+    return vol
+
+def vid2pngs(vid, prefix="image_"):
+    for t in vid.shape[2]:
+        vigra.impex.writeImage(vid[...,t],"tmp/%s%03d.png" % (t,))
     
 def ex1():
     vol = readvid()
-    meanim = meanimage(vol)
-    medim = medianimage(vol)
-    
+    meanim = np.mean(vol,axis=2)
+    medim = np.median(vol, axis=2)
+    '''
     plot.subplot(1,2,1)
     plot.imshow(meanim.swapaxes(0,1))
     plot.gray()
+    plot.title("Mean Image")
     
     plot.subplot(1,2,2)
     plot.imshow(medim.swapaxes(0,1))
     plot.gray()
-    
+    plot.title("Median Image")
+        
     plot.show()
+    '''
+    
+    volmean = vol-meanim.reshape((meanim.shape[0], meanim.shape[1],1)) + 255
+    volmed = vol-medim.reshape((medim.shape[0], medim.shape[1],1)) + 255
+    
+    volmean = volmean / max(volmean.max(),volmed.max()) * 255
+    volmed = volmed / max(volmean.max(),volmed.max()) * 255
+    
+    vid = np.concatenate((volmean,volmed))
+    
+    vid2pngs(vid)
+    
 if __name__ == "__main__":
     pass
     ex1()
