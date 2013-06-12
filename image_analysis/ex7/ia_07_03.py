7db8835442d11108a29d7697fa797ff70512de2b
Markus Döring <webmaster@burgerdev.de>
2013-06-12 11:12:07 +0200

    iman 07 ready

--- image_analysis/ex7/ia_07_03.py
+++ image_analysis/ex7/ia_07_03.py
@@ -20,6 +20,8 @@ def ex3():
     
     xs = np.linspace(0,5)
     
+    fig = plot.figure(1, figsize=(10,10))
+    
     
     plot.hold(True)
     ys1 = .05*xs+3.28
@@ -43,13 +45,21 @@ def ex3():
     # plot target vector
     plot.plot(xs,cost, 'g', linewidth=3)
     
-    # mark solution
-    ilp, = plot.plot([3,],[2,],'ro', markersize=12)
-    lp, = plot.plot([14.08/3.83,], [0,], 'bo', markersize=12)
-    rlp, = plot.plot([3,],[0,],'go', markersize=12)
+    # mark solutions
+    ilp_sol = (3,2)
+    ilp, = plot.plot(ilp_sol[0], ilp_sol[1],'ro', markersize=7)
+    print("ILP: solution={}, gain = {}".format(ilp_sol, 4*ilp_sol[0] + 3*ilp_sol[1]))
     
+    xlp = (7.51-3.28)/(0.05+1.71)
+    lp_sol = (xlp, -1.71*xlp+7.51)
+    lp, = plot.plot(lp_sol[0],lp_sol[1], 'bo', markersize=7)
+    print("LP: solution={}, gain = {}".format(lp_sol, 4*lp_sol[0] + 3*lp_sol[1]))
     
-    
+    rlp_sol = (2,3)
+    rlp, = plot.plot(rlp_sol[0], rlp_sol[1],'go', markersize=7)
+    print("rounded LP: solution={} gain = {}".format(rlp_sol, 4*rlp_sol[0] + 3*rlp_sol[1]))
+
+    # adjust plot
     plot.axis([-1, 5, -1, 5])
     plot.legend([ilp,lp,rlp],['ILP solution', 'LP solution', 'rounded LP solution'])
     plot.grid()
